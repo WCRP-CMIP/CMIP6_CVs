b847d1ba3baf3333fb147688c24310fcdf5f3ca8
Paul J. Durack <durack1@llnl.gov>
2017-04-08 18:40:27 -0700

    html end tag update

--- src/json_to_html.py
+++ src/json_to_html.py
@@ -35,7 +35,7 @@ print >> fo, """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http:/
 <link rel="stylesheet" type="text/css" href="jquery.dataTables-1.10.13.min.css"/>
 <script type="text/javascript" src="jquery-3.2.1.slim.min.js"></script>
 <script type="text/javascript" charset="utf8" src="jquery.dataTables-1.10.13.min.js"></script>
-<script src="jquery.dataTables-1.10.13.min.js">
+<script>
 $(document).ready( function () {
     $('#table_id').DataTable();
     } );

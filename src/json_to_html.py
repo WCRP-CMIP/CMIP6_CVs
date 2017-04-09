289affbb09696dab4ccef2f6d4a55cbb35a0ccc8
Paul J. Durack <durack1@llnl.gov>
2017-04-08 18:53:14 -0700

    html end tag update

--- src/json_to_html.py
+++ src/json_to_html.py
@@ -32,14 +32,17 @@ fo = open(fout, 'w')
 print >> fo, """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-<link rel="stylesheet" type="text/css" charset="utf8" href="jquery.dataTables-1.10.13.min.css"/>
+<link rel="stylesheet" type="text/css" charset="utf8" href="jquery.dataTables-1.10.13.min.css" />
 <script type="text/javascript" charset="utf8" src="jquery-3.2.1.slim.min.js"></script>
 <script type="text/javascript" charset="utf8" src="jquery.dataTables-1.10.13.min.js"></script>
-<script>
+<script src="jquery.dataTables-1.10.13.min.js" type="text/javascript">
+//<![CDATA[
 $(document).ready( function () {
     $('#table_id').DataTable();
     } );
+//]]>
 </script>
+<title></title>
 </head>
 <body>
 <table id="table_id" class="display">"""

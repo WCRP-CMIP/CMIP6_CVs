d1f37a5ea19889560b6326b14dd05df4868f0690
Paul J. Durack <durack1@llnl.gov>
2017-04-21 16:48:52 -0700

    Clean up None instances in source_id

--- src/writeJson.py
+++ src/writeJson.py
@@ -695,11 +695,11 @@ for jsonName in masterTargets:
                 if type(values[1]) is list:
                     print 'elif list'
                     print values[1]
-                    for count in range(0,len(values[1])):
+                    for count,ind in enumerate(range(0,len(values[1]))):
                         print count
-                        string = dictToClean[key][values[1]][count]
+                        string = dictToClean[key][values[1]][ind]
                         string = cleanString(string) ; # Clean string
-                        dictToClean[key][values[1]][count] = string
+                        dictToClean[key][values[1]][ind] = string
                 elif type(values[1]) is dict:
                     print 'elif dict'
                     # determine dict depth

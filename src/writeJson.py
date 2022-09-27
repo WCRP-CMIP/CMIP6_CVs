67627a40f8508647bfb060aef67834a0623aa354
Paul J. Durack <me@pauldurack.com>
2022-09-27 16:42:36 -0700

    Revised FIO-ESM-2-0 source_id license history

--- src/writeJson.py
+++ src/writeJson.py
@@ -620,6 +620,7 @@ PJD  1 Sep 2022    - Revised 4 IPSL* source_id license histories https://github.
 PJD 27 Sep 2022    - Revised IITM-ESM source_id license history https://github.com/WCRP-CMIP/CMIP6_CVs/issues/1149
 PJD 27 Sep 2022    - Revised 3 INM* source_id license histories https://github.com/WCRP-CMIP/CMIP6_CVs/issues/1050
 PJD 27 Sep 2022    - Revised 2 CCCma source_id license histories https://github.com/WCRP-CMIP/CMIP6_CVs/issues/1051
+PJD 27 Sep 2022    - Revised FIO-ESM-2-0 source_id license history https://github.com/WCRP-CMIP/CMIP6_CVs/issues/1155
                      - TODO: Review all start/end_year pairs for experiments https://github.com/WCRP-CMIP/CMIP6_CVs/issues/845
                      - TODO: Generate table_id from dataRequest https://github.com/WCRP-CMIP/CMIP6_CVs/issues/166
 
@@ -627,7 +628,7 @@ PJD 27 Sep 2022    - Revised 2 CCCma source_id license histories https://github.
 """
 
 # %% Set commit message and author info
-commitMessage = '\"evised 2 CCCma source_id license histories\"'
+commitMessage = '\"Revised FIO-ESM-2-0 source_id license history\"'
 #author = 'Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>'
 #author_institution_id = 'MOHC'
 author = 'Paul J. Durack <durack1@llnl.gov>'
@@ -1086,21 +1087,16 @@ source_id = source_id.get('source_id')  # Fudge to extract duplicate level
 del(tmp)
 
 # Fix issues
-source_ids_to_relax_list = [
-    "CanESM5",
-    "CanESM5-CanOE",
-]
-
-for key in source_ids_to_relax_list:
-    print("processing:", key)
-    licenseId = "CC BY 4.0"
-    source_id[key]["license_info"]["history"] += "; 2022-09-27: relaxed to CC BY 4.0"
-    source_id[key]["license_info"]["id"] = licenseId
-    licenseStr = license["license_options"][licenseId]["license_id"]
-    licenseUrl = license["license_options"][licenseId]["license_url"]
-    source_id[key]["license_info"]["license"] = "".join(
-        [licenseStr, " (", licenseId, "; ", licenseUrl, ")"])
-    source_id[key]["license_info"]["url"] = licenseUrl
+key = "FIO-ESM-2-0"
+print("processing:", key)
+licenseId = "CC BY 4.0"
+source_id[key]["license_info"]["history"] += "; 2022-09-27: relaxed to CC BY 4.0"
+source_id[key]["license_info"]["id"] = licenseId
+licenseStr = license["license_options"][licenseId]["license_id"]
+licenseUrl = license["license_options"][licenseId]["license_url"]
+source_id[key]["license_info"]["license"] = "".join(
+    [licenseStr, " (", licenseId, "; ", licenseUrl, ")"])
+source_id[key]["license_info"]["url"] = licenseUrl
 
 # Example license update, including email
 # source_ids_to_relax_list = [
